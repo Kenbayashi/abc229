d1720588c28623b494b32589a94c9246449704b7
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-18 15:57:56 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: Chars,
+        b: Chars,
+    }
+
+    let a = a.into_iter()
+             .map(|c| c.to_string().parse::<u8>().ok().unwrap())
+             .collect::<Vec<u8>>();
+
+    let b = b.into_iter()
+             .map(|c| c.to_string().parse::<u8>().ok().unwrap())
+             .collect::<Vec<u8>>();
+
+    let ans = a.into_iter()
+               .rev()
+               .zip(b.into_iter().rev())
+               .map(|(left, right)| left + right)
+               .all(|num| num < 10);
+
+    println!("{}", if ans {"Easy"} else {"Hard"});
 }
