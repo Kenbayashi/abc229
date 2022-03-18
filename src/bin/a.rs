44aea674cef578ca7541d262f1e7773d11145cb3
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-18 15:39:13 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,14 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        s1: String,
+        s2: String,
+    };
+
+    let check = |a: &String, b: &String| {
+        a == ".#" && b == "#."
+    };
+
+    let ans = check(&s1, &s2) || check(&s2, &s1);
+
+    println!("{}", if ans {"No"} else {"Yes"});
 }
