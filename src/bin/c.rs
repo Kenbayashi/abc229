e58066cab1bd99cc5d230190fe41ef4d2920557f
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-18 16:17:19 +0900

    c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,3 +1,20 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        w: i64,
+        mut cheese: [(i64, i64); n],
+    };
+
+    cheese.sort_by_key(|&(a, _)| -a);
+
+    let (ans, _) = cheese.into_iter()
+                         .fold((0, w), |(count, remain), (deli, weight)| {
+                             if weight <= remain {
+                                 (count + deli * weight, remain - weight)
+                             } else {
+                                 (count + deli * remain, 0)
+                             }
+                         });
+
+    println!("{}", ans);
 }
